ea9b38984f963f63718d9ef69c9e764ffcae6287
Kevin Belmont280 <juanbell289@gmail.com>
2024-10-30 19:49:49 -0600

    subir proyecto

---
+++ Evidencia 3/Evidencia 3/Data/Database.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Evidencia_3.Data
+{
+    internal class Database
+    {
+    }
+}
---
+++ Evidencia 3/Evidencia 3/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Evidencia_3.Models
+{
+    internal class User
+    {
+    }
+}
